d91402ec05652b9d8b22a925c8d6e8defff52bab
Georgiy Belyanin <belyaningeorge@ya.ru>
2025-02-01 15:58:44 +0300

    Implement Tarantool iproto hello world
    This patch implements basic iproto connection test. The app connects to
    local Tarantool instance and pings it using Tarantool-c [^1] library
    through FFI and `ocaml-ctypes` [^2] bindings.
    
    Run Tarantool and configure it as follows.
    ```lua
    box.cfg{listen = 'localhost:3301'}
    ```
    
    Then run the app. The ping should be successful.
    
    Note, `libffi` and `libtarantool` [^1] should be installed for the app
    to be built.
    
    [^1] https://github.com/tarantool/tarantool-c
    [^2] https://github.com/yallop/ocaml-ctypes

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,17 @@
 open Tarantool
 
-let () = print_endline "Hello, World!"
+let () =
+  let tnt =
+    Tnt.net
+      (Ctypes.coerce (Ctypes.ptr Ctypes.void) (Ctypes.ptr Tnt.stream)
+         Ctypes.null )
+  in
+  let _a = Tnt.set_string tnt Tnt.Uri "localhost:3301" in
+  let _b = Tnt.set_int tnt Tnt.SendBuf 0 in
+  let _c = Tnt.set_int tnt Tnt.RecvBuf 0 in
+  let _d = Tnt.connect tnt in
+  let _e = Tnt.ping tnt in
+  let reply = Tnt.reply_init Ctypes.null in
+  let f = Tnt.read_reply tnt reply in
+  if f == 0 then Format.printf "Ping success\n%!"
+  else Format.printf "Ping failed\n%!"
